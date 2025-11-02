07fe51248aca073c29a8f506f0b9e0f08dc0db3f
Tyler Jones <tyler.jones@uvu.edu>
2025-11-01 20:43:24 -0600

    basic changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+let ev = reactive({});
+
+window.addEventListener("devicemotion", (event) => {
+  ev = event;
+})
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>devicemotion</h1>
+  <pre>
+    {{ JSON.stringify(ev) }}
+  </pre>
 </template>
 
 <style scoped></style>
